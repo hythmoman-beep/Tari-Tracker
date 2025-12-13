17f19d2b88357853db8fe2d9e80b07b1e6588538
hythmoman-beep <hythmoman@gmail.com>
2025-12-14 00:52:26 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'tari-tracker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+  // Add './icon-192.png' here if you have the image
+];
+
+// Install Event (Cache files)
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Event (Serve from cache if offline)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+// Activate Event (Clean old caches)
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
